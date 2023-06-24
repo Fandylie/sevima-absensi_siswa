4b557438c9c97d7db766c48231ea3903baf50f1d
fandylie <fandiadisaputra53@gmail.com>
2023-06-24 15:30:09 +0800

    model dan migrate siswa

--- app/Models/tb_siswa.php
+++ app/Models/tb_siswa.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class tb_siswa extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'nisn',
+        'nis',
+        'nama',
+        'kelas',
+        'email',
+        'password',
+        'kelas',
+        'jeniskelamin',
+        'agama',
+        'tempatlahir',
+        'tanggallahir',
+        'tahunmasuk',
+    ];
 }
--- database/migrations/2023_06_24_051815_create_tb_siswas_table.php
+++ database/migrations/2023_06_24_051815_create_tb_siswas_table.php
@@ -14,7 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tb_siswas', function (Blueprint $table) {
-            $table->id();
+            $table->string('nisn', 11)->primary();
+            $table->string('nis', 15)->uniqid();
+            $table->string('nama', 100);
+            $table->string('email')->nullable();
+            $table->string('password', 225);
+            $table->string('kelas', 15);
+            $table->string('jeniskelamin', 20)->nullable();
+            $table->string('agama', 10)->nullable();
+            $table->string('tempatlahir', 100);
+            $table->date('tanggallahir'); 
+            $table->date('tahunmasuk');
             $table->timestamps();
         });
     }
